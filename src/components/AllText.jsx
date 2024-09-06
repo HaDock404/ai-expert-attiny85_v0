ca8eeb056c001de5ef50c05697e5545e3b2f41f9
HaDock404 <delescluse.g@gmail.com>
2024-09-06 19:56:13 +0200

    translate text

--- src/components/AllText.jsx
+++ src/components/AllText.jsx
@@ -12,11 +12,11 @@ export const intro_quote = {
 };
 
 export const title_01 = {
-    en: "Présentation BadUSB",
+    en: "BadUSB Presentation",
     fr: "Présentation BadUSB"
 };
 export const text_01 = {
-    en: "Une BadUSB est un périphérique USB modifié ou programmé pour exécuter des actions malveillantes sur un ordinateur une fois connecté. Elle exploite une vulnérabilité au niveau du firmware des périphériques USB, qui peut être reprogrammé pour se comporter différemment de ce qu'il est censé faire. Contrairement aux clés USB ordinaires, qui stockent et transfèrent des fichiers, une BadUSB se fait passer pour un périphérique de confiance tel qu'un clavier ou une souris, lui permettant ainsi d'exécuter des commandes ou des scripts automatiquement dès qu'elle est branchée sur un ordinateur. Cela peut inclure l'ouverture de ports réseau, le téléchargement de logiciels malveillants, ou même le vol de données sensibles. Ce type d'attaque est particulièrement dangereux car il est presque indétectable par les systèmes de sécurité traditionnels, puisque les périphériques USB sont généralement considérés comme sûrs par les systèmes d'exploitation. De plus, une fois que le firmware d'un périphérique a été modifié, il est très difficile, voire impossible, de revenir à son état d'origine sans outils spécifiques. Les BadUSB sont souvent utilisées par des attaquants ayant un accès physique à un ordinateur, mais elles peuvent également être introduites dans des environnements sécurisés à travers des campagnes de phishing ou en étant laissées dans des lieux publics pour être ramassées et utilisées par des victimes non méfiantes. La meilleure façon de se protéger contre ce type de menace est de restreindre l'accès aux ports USB, d'utiliser des périphériques USB de confiance uniquement, et de désactiver l'autorun pour empêcher l'exécution automatique de programmes dès que l'USB est branchée.",
+    en: "A BadUSB is a modified or programmed USB device designed to execute malicious actions on a computer once connected. It exploits a vulnerability in the firmware of USB devices, which can be reprogrammed to behave differently from what it is supposed to do. Unlike regular USB drives that store and transfer files, a BadUSB impersonates a trusted device such as a keyboard or mouse, allowing it to execute commands or scripts automatically as soon as it is plugged into a computer. This can include opening network ports, downloading malware, or even stealing sensitive data. This type of attack is particularly dangerous because it is almost undetectable by traditional security systems, as USB devices are generally considered safe by operating systems. Additionally, once the firmware of a device has been altered, it is very difficult, if not impossible, to restore it to its original state without specific tools. BadUSBs are often used by attackers with physical access to a computer, but they can also be introduced into secure environments through phishing campaigns or by being left in public places to be picked up and used by unsuspecting victims. The best way to protect against this type of threat is to restrict access to USB ports, only use trusted USB devices, and disable autorun to prevent the automatic execution of programs when the USB is connected.",
     fr: "Une BadUSB est un périphérique USB modifié ou programmé pour exécuter des actions malveillantes sur un ordinateur une fois connecté. Elle exploite une vulnérabilité au niveau du firmware des périphériques USB, qui peut être reprogrammé pour se comporter différemment de ce qu'il est censé faire. Contrairement aux clés USB ordinaires, qui stockent et transfèrent des fichiers, une BadUSB se fait passer pour un périphérique de confiance tel qu'un clavier ou une souris, lui permettant ainsi d'exécuter des commandes ou des scripts automatiquement dès qu'elle est branchée sur un ordinateur. Cela peut inclure l'ouverture de ports réseau, le téléchargement de logiciels malveillants, ou même le vol de données sensibles. Ce type d'attaque est particulièrement dangereux car il est presque indétectable par les systèmes de sécurité traditionnels, puisque les périphériques USB sont généralement considérés comme sûrs par les systèmes d'exploitation. De plus, une fois que le firmware d'un périphérique a été modifié, il est très difficile, voire impossible, de revenir à son état d'origine sans outils spécifiques. Les BadUSB sont souvent utilisées par des attaquants ayant un accès physique à un ordinateur, mais elles peuvent également être introduites dans des environnements sécurisés à travers des campagnes de phishing ou en étant laissées dans des lieux publics pour être ramassées et utilisées par des victimes non méfiantes. La meilleure façon de se protéger contre ce type de menace est de restreindre l'accès aux ports USB, d'utiliser des périphériques USB de confiance uniquement, et de désactiver l'autorun pour empêcher l'exécution automatique de programmes dès que l'USB est branchée."
 };
 
@@ -25,46 +25,46 @@ export const title_02 = {
     fr: "ATTiny85"
 };
 export const text_02 = {
-    en: "L'ATTiny85 est un microcontrôleur 8 bits de la famille AVR développé par Atmel (désormais Microchip Technology) qui se distingue par sa petite taille et sa polyvalence. Bien qu'il soit compact, avec seulement 8 broches, il est extrêmement puissant pour les projets d'électronique embarquée nécessitant une faible consommation d'énergie et un espace réduit. L'ATTiny85 est équipé de 8 Ko de mémoire flash, 512 octets de SRAM et 512 octets d'EEPROM, ce qui en fait un choix idéal pour des applications simples mais efficaces, comme la gestion de capteurs, la commande de moteurs ou même des projets de domotique. Grâce à sa capacité à être programmé via l'IDE Arduino, il est accessible aux amateurs et aux professionnels de l'électronique, facilitant son intégration dans une multitude de projets. Il peut être alimenté par une tension allant de 2,7 V à 5,5 V, ce qui lui permet de fonctionner aussi bien sur des piles que sur des alimentations classiques, rendant son usage flexible dans différents contextes. Bien qu'il ait moins de broches qu'un Arduino traditionnel, comme l'UNO, cela ne le limite pas forcément, car il supporte des fonctionnalités comme la communication I2C, SPI, et PWM, et peut utiliser des bibliothèques optimisées pour étendre ses capacités. Une autre de ses forces réside dans sa faible consommation d'énergie, ce qui le rend idéal pour des projets nécessitant une alimentation autonome sur le long terme, comme les dispositifs portables ou les objets connectés. Il est souvent utilisé dans des projets créatifs où l'espace est limité, comme les wearables ou les systèmes intégrés discrets. Cependant, son faible coût et sa simplicité d'utilisation le rendent tout aussi populaire dans l'enseignement et les projets de prototypage rapide.",
+    en: "The ATTiny85 is an 8-bit microcontroller from the AVR family developed by Atmel (now Microchip Technology), notable for its small size and versatility. Although compact, with only 8 pins, it is extremely powerful for embedded electronics projects requiring low power consumption and minimal space. The ATTiny85 is equipped with 8 KB of flash memory, 512 bytes of SRAM, and 512 bytes of EEPROM, making it an ideal choice for simple yet efficient applications, such as sensor management, motor control, or even home automation projects. Thanks to its ability to be programmed via the Arduino IDE, it is accessible to both electronics enthusiasts and professionals, making it easy to integrate into a wide range of projects. It can be powered by a voltage ranging from 2.7V to 5.5V, allowing it to operate on batteries as well as standard power supplies, offering flexibility in various contexts. Although it has fewer pins than a traditional Arduino, like the UNO, this doesn’t necessarily limit its capabilities, as it supports features like I2C, SPI, and PWM communication and can use optimized libraries to extend its functions. Another of its strengths is its low power consumption, making it ideal for projects requiring long-term autonomous power, such as portable devices or connected objects. It is often used in creative projects where space is limited, such as wearables or discreet embedded systems. However, its low cost and ease of use also make it popular in education and rapid prototyping projects.",
     fr: "L'ATTiny85 est un microcontrôleur 8 bits de la famille AVR développé par Atmel (désormais Microchip Technology) qui se distingue par sa petite taille et sa polyvalence. Bien qu'il soit compact, avec seulement 8 broches, il est extrêmement puissant pour les projets d'électronique embarquée nécessitant une faible consommation d'énergie et un espace réduit. L'ATTiny85 est équipé de 8 Ko de mémoire flash, 512 octets de SRAM et 512 octets d'EEPROM, ce qui en fait un choix idéal pour des applications simples mais efficaces, comme la gestion de capteurs, la commande de moteurs ou même des projets de domotique. Grâce à sa capacité à être programmé via l'IDE Arduino, il est accessible aux amateurs et aux professionnels de l'électronique, facilitant son intégration dans une multitude de projets. Il peut être alimenté par une tension allant de 2,7 V à 5,5 V, ce qui lui permet de fonctionner aussi bien sur des piles que sur des alimentations classiques, rendant son usage flexible dans différents contextes. Bien qu'il ait moins de broches qu'un Arduino traditionnel, comme l'UNO, cela ne le limite pas forcément, car il supporte des fonctionnalités comme la communication I2C, SPI, et PWM, et peut utiliser des bibliothèques optimisées pour étendre ses capacités. Une autre de ses forces réside dans sa faible consommation d'énergie, ce qui le rend idéal pour des projets nécessitant une alimentation autonome sur le long terme, comme les dispositifs portables ou les objets connectés. Il est souvent utilisé dans des projets créatifs où l'espace est limité, comme les wearables ou les systèmes intégrés discrets. Cependant, son faible coût et sa simplicité d'utilisation le rendent tout aussi populaire dans l'enseignement et les projets de prototypage rapide."
 };
 export const text_02bis = {
-    en: "L'ATTiny85 coûte en moyenne 1$. Vous pouvez vous en procurez très facilement sur AliExpress.",
+    en: "The ATTiny85 costs on average $1. You can easily purchase it on AliExpress.",
     fr: "L'ATTiny85 coûte en moyenne 1$. Vous pouvez vous en procurez très facilement sur AliExpress."
 };
 
 export const title_03 = {
-    en: "Installation de l'IDE Arduino",
+    en: "Installation of the Arduino IDE",
     fr: "Installation de l'IDE Arduino"
 };
 export const text_03 = {
-    en: "Ce tutoriel s'adresse principalement aux utilisateurs de macOS. Pour les autres systèmes d'exploitation, les étapes d'installation et de programmation sont similaires et peuvent être facilement trouvées en ligne ou via des outils comme ChatGPT. Pour commencer, rendez-vous sur le site officiel d'Arduino à l'adresse suivante : https://www.arduino.cc/en/software et téléchargez la version adaptée à macOS. Au moment de la rédaction de ce tutoriel, la dernière version disponible est la 2.3.2.",
+    en: "This tutorial is primarily intended for macOS users. For other operating systems, the installation and programming steps are similar and can be easily found online or through tools like ChatGPT. To get started, visit the official Arduino website at the following address: https://www.arduino.cc/en/software and download the version suitable for macOS. At the time of writing this tutorial, the latest available version is 2.3.2.",
     fr: "Ce tutoriel s'adresse principalement aux utilisateurs de macOS. Pour les autres systèmes d'exploitation, les étapes d'installation et de programmation sont similaires et peuvent être facilement trouvées en ligne ou via des outils comme ChatGPT. Pour commencer, rendez-vous sur le site officiel d'Arduino à l'adresse suivante : https://www.arduino.cc/en/software et téléchargez la version adaptée à macOS. Au moment de la rédaction de ce tutoriel, la dernière version disponible est la 2.3.2."
 };
 export const text_03bis = {
-    en: "Localisez le fichier d'installation dans votre dossier 'Téléchargements', puis double-cliquez dessus pour lancer l'installation. Suivez ensuite les instructions à l'écran pour terminer l'installation de l'IDE.",
+    en: "Locate the installation file in your 'Downloads' folder, then double-click it to start the installation. Follow the on-screen instructions to complete the installation of the IDE.",
     fr: "Localisez le fichier d'installation dans votre dossier 'Téléchargements', puis double-cliquez dessus pour lancer l'installation. Suivez ensuite les instructions à l'écran pour terminer l'installation de l'IDE."
 };
 
 export const title_04 = {
-    en: "Mise à jour des bibliothèques",
+    en: "Updating Libraries",
     fr: "Mise à jour des bibliothèques"
 };
 export const text_04 = {
-    en: "Pour utiliser la bibliothèque correspondant à l'ATTiny85, vous devez d'abord mettre à jour les bibliothèques disponibles dans l'IDE Arduino. Pour cela, allez dans 'Préférences' et entrez l'adresse suivante dans le champ prévu à cet effet, comme illustré : https://raw.githubusercontent.com/ArminJo/DigistumpArduino/master/package_digistump_index.json.",
+    en: "To use the library for the ATTiny85, you first need to update the available libraries in the Arduino IDE. To do this, go to 'Preferences' and enter the following address in the designated field, as shown: https://raw.githubusercontent.com/ArminJo/DigistumpArduino/master/package_digistump_index.json.",
     fr: "Pour utiliser la bibliothèque correspondant à l'ATTiny85, vous devez d'abord mettre à jour les bibliothèques disponibles dans l'IDE Arduino. Pour cela, allez dans 'Préférences' et entrez l'adresse suivante dans le champ prévu à cet effet, comme illustré : https://raw.githubusercontent.com/ArminJo/DigistumpArduino/master/package_digistump_index.json."
 };
 export const text_04bis = {
-    en: "Cliquez ensuite sur 'OK' pour confirmer.",
+    en: "Then click 'OK' to confirm.",
     fr: "Cliquez ensuite sur 'OK' pour confirmer."
 };
 
 export const title_05 = {
-    en: "Installation de la bibliothèque Digistump AVR Board",
+    en: "Installing the Digistump AVR Board Library",
     fr: "Installation de la bibliothèque Digistump AVR Board"
 };
 export const text_05 = {
-    en: "Dans le menu 'Outils', ouvrez le Gestionnaire de cartes, recherchez Digistump AVR Boards, puis procédez à son installation.",
+    en: "In the 'Tools' menu, open the Board Manager, search for Digistump AVR Boards, and then proceed with its installation.",
     fr: "Dans le menu 'Outils', ouvrez le Gestionnaire de cartes, recherchez Digistump AVR Boards, puis procédez à son installation."
 };
 export const text_05bis = {
@@ -73,24 +73,24 @@ export const text_05bis = {
 };
 
 export const title_06 = {
-    en: "Importation des bibliothèques",
+    en: "Importing Libraries",
     fr: "Importation des bibliothèques"
 };
 export const text_06 = {
-    en: "Avant de commencer à coder, il est essentiel d'importer les bibliothèques que vous avez installées précédemment pour l'ATTiny85. Pour cela, allez dans la section 'Croquis' de l'IDE Arduino, puis sélectionnez 'Importer bibliothèque'. Choisissez ensuite la bibliothèque correspondante : DigisparkKeyboard.",
+    en: "Before you start coding, it is essential to import the libraries you previously installed for the ATTiny85. To do this, go to the 'Sketch' section of the Arduino IDE, then select 'Include Library'. Next, choose the corresponding library: DigisparkKeyboard.",
     fr: "Avant de commencer à coder, il est essentiel d'importer les bibliothèques que vous avez installées précédemment pour l'ATTiny85. Pour cela, allez dans la section 'Croquis' de l'IDE Arduino, puis sélectionnez 'Importer bibliothèque'. Choisissez ensuite la bibliothèque correspondante : DigisparkKeyboard."
 };
 export const text_06bis = {
-    en: "Vous verez alors apparaitre dans votre interface de code les différentes bibliothèques nécessaires.",
-    fr: "Vous verez alors apparaitre dans votre interface de code les différentes bibliothèques nécessaires."
+    en: "You will then see the various required libraries appear in your code interface.",
+    fr: "Vous verrez alors apparaitre dans votre interface de code les différentes bibliothèques nécessaires."
 };
 
 export const title_07 = {
-    en: "Exemple de code",
-    fr: "Exemple de code"
+    en: "Code Example",
+    fr: "Exemple de Code"
 };
 export const text_07 = {
-    en: "Voici un exemple de code simple qui fonctionne et ne cause aucun dégat à votre machine.",
+    en: "Here is an example of a simple code that works and does no harm to your machine.",
     fr: "Voici un exemple de code simple qui fonctionne et ne cause aucun dégat à votre machine."
 };
 export const text_07bis = {
@@ -99,24 +99,24 @@ export const text_07bis = {
 };
 
 export const title_08 = {
-    en: "Téléversement du code",
+    en: "Uploading the Code",
     fr: "Téléversement du code"
 };
 export const text_08 = {
-    en: "Le téléversement du code se fait par le biais de la flèche tourné vers la droite en haut à gauche de votre IDE. Vous avez alors 60secondes pour brancher l'ATTiny85 avec votre machine.",
+    en: "Uploading the code is done via the rightward arrow at the top left of your IDE. You then have 60 seconds to connect the ATTiny85 to your machine.",
     fr: "Le téléversement du code se fait par le biais de la flèche tourné vers la droite en haut à gauche de votre IDE. Vous avez alors 60secondes pour brancher l'ATTiny85 avec votre machine."
 };
 export const text_08bis = {
-    en: "Retirez rapidement votre ATTiny85 à la fin du téléversement, celui ci voudra immédiatement injecter son programme sur votre machine. Il est préférable de le tester sur un page vierge de textEdit ou tout autre outil de traitement de texte.",
+    en: "Quickly remove your ATTiny85 at the end of the upload, as it will immediately attempt to inject its program into your machine. It is best to test it on a blank page in TextEdit or any other text editing tool.",
     fr: "Retirez rapidement votre ATTiny85 à la fin du téléversement, celui ci voudra immédiatement injecter son programme sur votre machine. Il est préférable de le tester sur un page vierge de textEdit ou tout autre outil de traitement de texte."
 };
 
 export const title_09 = {
-    en: "MAJ ATTiny85",
+    en: "Update ATTiny85",
     fr: "MAJ ATTiny85"
 };
 export const text_09 = {
-    en: "Il peut être parfois important de mettre à jour l'ATTiny85, pour cela suivez les instructions ci dessous.",
+    en: "It may sometimes be important to update the ATTiny85; for this, follow the instructions below.",
     fr: "Il peut être parfois important de mettre à jour l'ATTiny85, pour cela suivez les instructions ci dessous."
 };
 export const text_09bis = {
@@ -125,11 +125,11 @@ export const text_09bis = {
 };
 
 export const title_10 = {
-    en: "Remise à zéro",
+    en: "Resetting",
     fr: "Remise à zéro"
 };
 export const text_10 = {
-    en: "Pour remettre l'ATTiny85 à zéro, suivez les instructions ci-dessous.",
+    en: "To reset the ATTiny85, follow the instructions below.",
     fr: "Pour remettre l'ATTiny85 à zéro, suivez les instructions ci-dessous."
 };
 export const text_10bis = {
