b9aec784f1bc629f7ebfdb3a2a2f0317a29fc1db
HaDock404 <delescluse.g@gmail.com>
2024-09-06 07:24:10 +0200

    dropdown language

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import '../styles/header.css'
+import Dropdown from './Dropdown'
+import Worldicon from './Worldicon'
 
 function Header(props) {
     return (
         <header id="header">
             {props.title}
+            <div className='lang-menu'>
+                <Worldicon />
+                <Dropdown />
+            </div>
         </header>
     )
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,15 +2,16 @@ import GlobalStyle from '../styles/createGlobalStyle.jsx'
 import Header from "../components/Header";
 import Main from '../components/Main.jsx';
 import Footer from '../components/Footer.jsx';
+import { LanguageProvider } from '../components/LanguageContext';
 
 function Dashboard() {
   return (
-    <>
+    <LanguageProvider>
       <GlobalStyle/>
       <Header title="Création d'une BadUSB avec un ATTiny85"/>
       <Main />
       <Footer />
-    </>
+    </LanguageProvider>
   );
 }
 
